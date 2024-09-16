c32a02d58adbfe053e476fc6bdab673663e54b2f
SDilhara19 <sdilharabandara@gmail.com>
2024-09-16 22:45:15 +0530

    student_chat with history

--- service/Chatbot/lang_funcs.py
+++ service/Chatbot/lang_funcs.py
@@ -10,6 +10,7 @@ from langchain_core.prompts import MessagesPlaceholder
 from langchain.chains import create_history_aware_retriever
 from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_google_datastore import DatastoreChatMessageHistory
+from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.messages import HumanMessage, AIMessage
 from google.cloud import datastore
 from google.oauth2 import service_account
@@ -18,8 +19,19 @@ from langchain_core.messages import (
     AIMessage as ImportedAIMessage,
 )
 import json
+import uuid
+from langchain_core.messages import BaseMessage, AIMessage
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.pydantic_v1 import BaseModel, Field
+from typing import List
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
 
-
+from langchain_core.runnables import (
+    RunnableLambda,
+    ConfigurableFieldSpec,
+)
 project_id = "helaedu-website"
 
 source = open("./config/credentials.json")
@@ -28,32 +40,28 @@ source.close()
 db_credentials = service_account.Credentials.from_service_account_info(info)
 db_client = datastore.Client(project=project_id, credentials=db_credentials)
 
+cred = credentials.Certificate(info)
+app = firebase_admin.initialize_app(cred)
+db = firestore.client()
 
-# This will load the PDF file
+def generate_chat_id():
+    #generates a unique chat id
+    chat_id = str(uuid.uuid4())  
+    return chat_id
+
+#load the PDF file
 def load_pdf_data(file_path):
-    # Creating a PyMuPDFLoader object with file_path
     loader = PyMuPDFLoader(file_path=file_path)
-
-    # loading the PDF file
     docs = loader.load()
-
-    # returning the loaded document
     return docs
 
 
-# Responsible for splitting the documents into several chunks
+#splitting the documents into several chunks
 def split_docs(documents, chunk_size=1000, chunk_overlap=200):
-
-    # Initializing the RecursiveCharacterTextSplitter with
-    # chunk_size and chunk_overlap
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=chunk_size, chunk_overlap=chunk_overlap
     )
-
-    # Splitting the documents into chunks
     chunks = text_splitter.split_documents(documents=documents)
-
-    # returning the document chunks
     return chunks
 
 
@@ -61,34 +69,26 @@ def split_docs(documents, chunk_size=1000, chunk_overlap=200):
 def load_embedding_model(model_path, normalize_embedding=True):
     return HuggingFaceEmbeddings(
         model_name=model_path,
-        model_kwargs={"device": "cpu"},  # here we will run the model with CPU only
+        model_kwargs={"device": "cpu"},
         encode_kwargs={
-            "normalize_embeddings": normalize_embedding  # keep True to compute cosine similarity
+            "normalize_embeddings": normalize_embedding  
         },
     )
 
 
 # Function for creating embeddings using FAISS
 def create_embeddings(chunks, embedding_model, storing_path="Chatbot/vectorstore"):
-    # Creating the embeddings using FAISS
     vectorstore = FAISS.from_documents(chunks, embedding_model)
-
-    # Saving the model in current directory
     vectorstore.save_local(storing_path)
-
-    # returning the vectorstore
     return vectorstore
 
 
 def load_vectorstore(embedding_model, storing_path="Chatbot/vectorstore/10"):
 
     embeddings = load_embedding_model(embedding_model)
-    # Loading the saved vector store
     vectorstore = FAISS.load_local(
         storing_path, embeddings, allow_dangerous_deserialization=True
     )
-
-    # Returning the retriever
     return vectorstore
 
 
@@ -117,19 +117,80 @@ def create_chain(history_aware_retriever, llm, qa_prompt):
     return rag_chain
 
 
-def create_conversational_chain(rag_chain, session_id):
+class ChatHistory(BaseChatMessageHistory, BaseModel):
+    """In memory implementation of chat message history."""
+    user_id: str 
+    chat_id: str
+    messages: List[BaseMessage] = Field(default_factory=list)
 
+    def add_messages(self, messages: List[BaseMessage]) -> None:
+        """Add a list of messages to the store"""
+        user_id = self.user_id
+        chat_id = self.chat_id  
+        self.messages.extend(messages)
+        message_contents = [{'content': message.content, 'type': message.type} for message in self.messages]
+        
+        try:
+        # Save to Firestore
+            db.collection("ChatHistory").document(chat_id).set({"userId": user_id,"messages": message_contents})
+        except Exception as e:
+            print(f"Error saving to Firestore: {e}")
+
+    def clear(self) -> None:
+        self.messages = []
+
+store = {}
+
+def get_session_history(
+    user_id: str, conversation_id: str
+) -> BaseChatMessageHistory:
+
+    if (user_id, conversation_id) not in store:
+        store[(user_id, conversation_id)] = ChatHistory(user_id=user_id, chat_id=conversation_id)
+
+    
+    return store[(user_id, conversation_id)]
+
+def create_conversational_chain(rag_chain, session_id):
+    
     return RunnableWithMessageHistory(
         rag_chain,
-        lambda session_id: DatastoreChatMessageHistory(session_id, client=db_client),
+        get_session_history= get_session_history,
         input_messages_key="input",
         history_messages_key="chat_history",
         output_messages_key="answer",
+        history_factory_config=[
+            ConfigurableFieldSpec(
+                id="user_id",
+                annotation=str,
+                name="User ID",
+                description="Unique identifier for the user.",
+                default="",
+                is_shared=True,
+            ),
+            ConfigurableFieldSpec(
+                id="conversation_id",
+                annotation=str,
+                name="Conversation ID",
+                description="Unique identifier for the conversation.",
+                default="",
+                is_shared=True,
+            ),
+        ],
     )
 
 def retrieve_history(session_id):
-    chat_history = DatastoreChatMessageHistory(session_id, client=db_client)
-    return serialize_chat_history(chat_history.messages)
+    doc_ref = db.collection("ChatHistory").document(session_id)
+    history = doc_ref.get()
+    document_data = history.to_dict()
+    messages = document_data.get('messages', []) 
+    if messages:
+        print(f"Document data: {messages}")
+        return messages
+    else:
+        print("No such document!")
+        return None
+   
 
 class HumanMessage:
     def __init__(self, content):
@@ -168,11 +229,11 @@ def get_references(context):
     return references
 
 
-def get_response(query, chain, user_session_id):
+def get_response(query, chain, user_session_id, user_id):
     # Getting response from chain
     response = chain.invoke(
         {"input": query},
-        config={"configurable": {"session_id": user_session_id}},
+        config={"configurable": {"user_id": user_id, "conversation_id": user_session_id}},
     )
 
     return response

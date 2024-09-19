03f6452b50c0aa438b93eac72139f9506f9844b6
LNTR <101091694+LNTR@users.noreply.github.com>
2024-09-19 16:51:47 +0530

    Clustering intergration to frontend Classifier data pickling Classifier & Clustering model creation

--- service/Blueprint/articles.py
+++ service/Blueprint/articles.py
@@ -2,7 +2,7 @@ from flask import Blueprint, request, jsonify
 from utils import authenticate
 from firebase_admin import firestore
 from Models.Articles import Article
-from Components.Articles import recreate_model as _recreate_model
+from Components.Articles import recreate_model as _recreate_model, _get_related_articles
 
 
 articles = Blueprint("articles", __name__)
@@ -14,11 +14,18 @@ def approve_article(article_id):
     article = Article.collection.get(article_id)
     article.approve()
     article.update()
-    return {}
+    return jsonify({"message": "update complete"})
 
 
 @articles.route("/recreate-model", methods=["GET"])
 @articles.route("/recreate-model/", methods=["GET"])
 def recreate_model():
     _recreate_model()
-    return {"articles": ""}
+    return jsonify({"message": "Model recreated"})
+
+
+@articles.route("/get-recommendation", methods=["GET"])
+@articles.route("/get-recommendation/<cluster>", methods=["GET"])
+def get_reccomendation(cluster):
+    articles = _get_related_articles(cluster)
+    return jsonify(articles)
--- service/Components/Articles/__init__.py
+++ service/Components/Articles/__init__.py
@@ -24,3 +24,20 @@ def recreate_model():
     df = create_df()
     create_clusters(df)
     create_classifier(df)
+
+
+def _get_related_articles(cluster):
+    articles_req = Article.collection.filter(cluster=cluster)
+    candidate_articles_req = articles_req.limit(4)
+    candidate_articles = candidate_articles_req.fetch()
+    data = []
+    for article in candidate_articles:
+        data.append(
+            {
+                "articleId": article.articleId,
+                "title": article.title,
+                "imageRef": article.imageRef,
+                "publishedpublishedTimestamp": article.publishedTimestamp,
+            }
+        )
+    return data

05c1db658f9332771aa00d6ef241dee3fa75dab2
SDilhara19 <sdilharabandara@gmail.com>
2024-10-13 23:33:47 +0530

    add resource

--- client/src/pages/moderator/Resources.jsx
+++ client/src/pages/moderator/Resources.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Header } from '@components/common'
 import Sidebar from '@components/moderator_com/ModeratorSidebar'
 import { useState } from 'react'
+import Topic from '@components/moderator_com/Topic';
+import AddResource from '@components/moderator_com/AddResource';
 
 export default function Resources() {
     const [sidebar, setSidebar] = useState(false);
@@ -12,9 +14,9 @@ export default function Resources() {
         <Header />
         <div className="dashboard h-screen mx-auto" style={{ width: sidebar ? `calc(100vw - 384px)` : '100vw' }} onClick={() => setSidebar(false)}>
           <Sidebar value={sidebar} setValue={setSidebar} />
-          <div className="content-wrapper mx-64 border" >
-            <h1 className="mx-10 my-14">Resources
-            </h1>
+          <div className="content-wrapper mx-64" >
+            <Topic topic='Resources' description='Upload updated or newly published textbooks and teachers resources' />
+            <AddResource />
           </div>
         </div>
   

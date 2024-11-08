dc5bdfbc4cf016a05658928403f93642a81c35c4
LNTR <101091694+LNTR@users.noreply.github.com>
2024-11-08 10:00:24 +0530

    modified:   client/src/components/articles/AddArticlesForm.jsx 	modified:   client/src/components/articles/AddComment.jsx 	modified:   client/src/components/test/Test.jsx

--- client/src/components/articles/AddComment.jsx
+++ client/src/components/articles/AddComment.jsx
@@ -1,18 +1,17 @@
-import React, { useState } from 'react';
-import { addComment } from '@services/ArticleService';
-import useAuthHeader from "react-auth-kit/hooks/useAuthUser";
+import React, { useState } from "react";
+import { addComment } from "@services/ArticleService";
+import useAuthHeader from "react-auth-kit/hooks/useAuthHeader";
 
 export default function AddComment({ articleId }) {
-  
   const authHeader = useAuthHeader();
   const headers = {
     Authorization: authHeader,
   };
-  const [commentText, setCommentText] = useState('');
+  const [commentText, setCommentText] = useState("");
 
   const handlePostComment = async (e) => {
-    e.preventDefault(); 
-    if (!commentText.trim()) return; 
+    e.preventDefault();
+    if (!commentText.trim()) return;
     console.log(articleId);
     try {
       const comments = {
@@ -20,9 +19,10 @@ export default function AddComment({ articleId }) {
         articleId,
         parentId: 0,
       };
+      console.log(headers);
       const response = await addComment(comments, headers);
       if (response.status === 200) {
-        setCommentText('');
+        setCommentText("");
       }
     } catch (err) {
       if (err.response) {
@@ -32,26 +32,26 @@ export default function AddComment({ articleId }) {
       }
     }
   };
-  
 
   return (
-    <div className='m-12'>
+    <div className="m-12">
       <h1>Leave a Comment</h1>
-      <hr className="border-yellow border-t-4 w-96"></hr><br></br>
-      
+      <hr className="border-yellow border-t-4 w-96"></hr>
+      <br></br>
+
       <form onSubmit={handlePostComment}>
-        <input 
-          type="text" 
-          className='border border-blue w-11/12 h-80 rounded-xl mt-7 mb-7 px-10'
+        <input
+          type="text"
+          className="border border-blue w-11/12 h-80 rounded-xl mt-7 mb-7 px-10"
           value={commentText}
           onChange={(e) => setCommentText(e.target.value)}
           placeholder="Write your comment..."
         />
-        
-        <div className='flex justify-start'>
-          <button 
-            type="submit" 
-            className='bg-yellow text-white rounded-xl p-4 text-3xl'
+
+        <div className="flex justify-start">
+          <button
+            type="submit"
+            className="bg-yellow text-white rounded-xl p-4 text-3xl"
           >
             Post Comment
           </button>

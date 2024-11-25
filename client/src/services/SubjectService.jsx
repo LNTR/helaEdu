6f2d3b261770c8c98ab2c17571d03374cae5f669
geethansulochana <2021is036@stu.ucsc.cmb.ac.lk>
2024-11-25 08:32:37 +0530

    add comemnt list for subjects

--- client/src/services/SubjectService.jsx
+++ client/src/services/SubjectService.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const REST_API_BASE_URL = `${import.meta.env.VITE_REST_API_BASE_URL}/subjects`;
 const CREATE_SUBJECTS_URL = `${import.meta.env.VITE_REST_API_BASE_URL}/subjects/create`;
-
+const ADD_COMMENTS_SUBJECT_URL=`${import.meta.env.VITE_REST_API_BASE_URL}/forum/subject/create`;
 export const createSubject = (subjectsData) =>axios.post(`${CREATE_SUBJECTS_URL}`, subjectsData);
 
 export const uploadPdf = (subjectId, formData) =>axios.post(`${REST_API_BASE_URL}/uploadPdf`, formData, {
@@ -11,7 +11,9 @@ export const uploadPdf = (subjectId, formData) =>axios.post(`${REST_API_BASE_URL
     },
     params: { subjectId }
   });
-  export const listSubjectsByGrade = (grade) => 
-    axios.get(`${REST_API_BASE_URL}/byGrade/${grade}`);
+  export const listSubjectsByGrade = (grade) => axios.get(`${REST_API_BASE_URL}/byGrade/${grade}`);
+export const getCommentsBySubjectId =(subjectId)=>axios.get(`${REST_API_BASE_URL}/${subjectId}/comments`);
   
-  
+ 
+export const addCommentForForum = (comment, headers) =>
+  axios.post(ADD_COMMENTS_SUBJECT_URL, comment, { headers }); 

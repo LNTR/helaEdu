320b3dac57e233c58fe16919e5178ca63938416b
NirmalHettiarachchi <nirmalhettiarachchi5@gmail.com>
2024-11-30 12:28:55 +0530

    Badge

--- server/src/main/java/com/helaedu/website/controller/BadgeController.java
+++ server/src/main/java/com/helaedu/website/controller/BadgeController.java
@@ -18,19 +18,4 @@ public class BadgeController {
     public BadgeController(BadgeService badgeService){
         this.badgeService = badgeService;
     }
-    //add badges
-    @PostMapping("/createBadge")
-    public ResponseEntity<String> createBadge(@Valid @RequestBody BadgeDto badgeDto, BindingResult bindingResult) throws ExecutionException, InterruptedException {
-        if (bindingResult.hasErrors()) {
-            return new ResponseEntity<>(bindingResult.getFieldError().getDefaultMessage(), HttpStatus.BAD_REQUEST);
-        }
-        try {
-            String badgeId = badgeService.createBadge(badgeDto);
-            return new ResponseEntity<>(badgeId, HttpStatus.CREATED);
-        } catch (IllegalArgumentException e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-        } catch (ExecutionException | InterruptedException e) {
-            return new ResponseEntity<>("Error creating badge", HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
 }

e4ca32cc21e627738bfa666bbe9cff2984894c1b
LNTR <101091694+LNTR@users.noreply.github.com>
2024-12-01 16:03:11 +0530

    Subject note backend

--- client/src/components/subject/StickyNote.jsx
+++ client/src/components/subject/StickyNote.jsx
@@ -1,23 +1,49 @@
-import React, { useMemo } from "react";
-
+import React, { useMemo, useEffect, useState } from "react";
+import { viewNote, updateNote } from "@services/SubjectNoteService";
 import JoditEditor from "jodit-react";
+import useAuthHeader from "react-auth-kit/hooks/useAuthHeader";
 
-function StickyNote({subjectId}) {
+function StickyNote({ subjectId }) {
+  const authHeader = useAuthHeader();
+  const headers = {
+    Authorization: authHeader,
+  };
+
+  const [content, setContent] = useState("");
+
+  useEffect(() => {
+    viewNote(subjectId, headers).then((res) => {
+      setContent(res.data ? res.data.note_content : "");
+    });
+  }, [subjectId]);
+
   const config = useMemo(
     () => ({
       readonly: false,
-      placeholder: "Start typings...",
+      placeholder: "",
       height: "80vh",
     }),
     []
   );
+
   return (
     <>
-      <JoditEditor config={config} />
+      <JoditEditor
+        config={config}
+        value={content}
+        onChange={(newContent) => setContent(newContent)}
+      />
       <div className="flex justify-end my-4 mx-10">
-        <button type="submit" className="bg-blue px-2 py-1 text-lg text-white">Save Note</button>
+        <button
+          type="submit"
+          className="bg-blue px-2 py-1 text-lg text-white"
+          onClick={async () => {
+            await updateNote(subjectId, { content }, headers);
+          }}
+        >
+          Save Note
+        </button>
       </div>
-     
     </>
   );
 }
--- service/Blueprint/notes.py
+++ service/Blueprint/notes.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, request, jsonify
 from utils import authenticate
-from firebase_admin import firestore
 from Models.Student import Student
 
 
@@ -12,8 +11,11 @@ notes = Blueprint("notes", __name__)
 def view_subject_note(data, subject_id):
     try:
         email = data["sub"]
+        subject_id = subject_id.replace("-", "_").replace("/", "_")
         user: Student = Student.find_by_email(email)
-        note_content = user.subjectNoteList[subject_id]
+        note_content = ""
+        if subject_id in user.subjectNoteList.keys():
+            note_content = user.subjectNoteList[subject_id]
 
         return jsonify({"note_content": note_content})
     except Exception as e:
@@ -24,10 +26,17 @@ def view_subject_note(data, subject_id):
 @authenticate
 def update_subject_note(data, subject_id):
     try:
-        new_content = request.json()["content"]
+        subject_id = subject_id.replace("-", "_").replace("/", "_")
+        request_payload = request.get_json(silent=True)
+        new_content = request_payload["content"]
         email = data["sub"]
         user: Student = Student.find_by_email(email)
-        user.subjectNoteList[subject_id] = new_content
+        user.subjectNoteList = {
+            **dict(user.subjectNoteList),
+            subject_id: new_content,
+        }
+        user.update()
+
         return jsonify({"status": "success"})
 
     except Exception as e:
